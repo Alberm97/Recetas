11317ad522f258d5a20b79e5a30ca2c2c5b88887
Alber <albertomontero0697@gmail.com>
2023-11-08 19:04:27 +0100

    Terminado!!

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,142 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static final String XML_FILE = "RecetaDOM.xml";
+
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        File archivoXML = new File("C:\\Users\\Alber\\IdeaProjects\\Recetas\\src\\RecetaDOM.xml");
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(archivoXML);
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Menú Principal:");
+                System.out.println("1. Leer ficheros XML con ese formato.");
+                System.out.println("2. Añadir una nueva receta.");
+                System.out.println("3. Modificar el título de la receta.");
+                System.out.println("4. Eliminar la receta.");
+                System.out.println("0. Salir del programa.");
+                System.out.print("Seleccione una opción: ");
+                int opcion = scanner.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        // Lógica para leer el archivo XML y mostrar las recetas
+                        leerRecetas(doc);
+                        break;
+                    case 2:
+                        // Lógica para añadir una nueva receta
+                        agregarReceta(doc);
+                        break;
+                    case 3:
+                        // Lógica para modificar el título de la receta
+                        modificarTituloReceta(doc);
+                        break;
+                    case 4:
+                        // Lógica para eliminar la receta
+                        eliminarReceta(doc);
+                        break;
+                    case 0:
+                        guardarDocumento(doc);
+                        System.out.println("Saliendo del programa.");
+                        System.exit(0);
+                    default:
+                        System.out.println("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void leerRecetas(Document doc) {
+        NodeList recetas = doc.getElementsByTagName("Receta");
+        for (int i = 0; i < recetas.getLength(); i++) {
+            Element receta = (Element) recetas.item(i);
+            String titulo = receta.getElementsByTagName("titulo").item(0).getTextContent();
+            System.out.println("Título: " + titulo);
+            // Puedes mostrar otros detalles de la receta aquí
+        }
+    }
+
+    private static void agregarReceta(Document doc) {
+        Scanner scanner = new Scanner(System.in);
+        Element nuevaReceta = doc.createElement("Receta");
+        System.out.print("Ingrese el título de la nueva receta: ");
+        String titulo = scanner.nextLine();
+        Element tituloElement = doc.createElement("titulo");
+        tituloElement.appendChild(doc.createTextNode(titulo));
+        nuevaReceta.appendChild(tituloElement);
+        System.out.println("Receta agregada con éxito.");
+
+        // Lógica para ingresar ingredientes, procedimiento, tiempo, etc.
+
+        // Agregar la nueva receta al documento
+        doc.getDocumentElement().appendChild(nuevaReceta);
+    }
+
+    private static void modificarTituloReceta(Document doc) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el título de la receta que desea modificar: ");
+        String tituloAnterior = scanner.nextLine();
+
+        NodeList recetas = doc.getElementsByTagName("Receta");
+        for (int i = 0; i < recetas.getLength(); i++) {
+            Element receta = (Element) recetas.item(i);
+            String titulo = receta.getElementsByTagName("titulo").item(0).getTextContent();
+            if (titulo.equalsIgnoreCase(tituloAnterior)) {
+                System.out.print("Ingrese el nuevo título: ");
+                String nuevoTitulo = scanner.nextLine();
+                receta.getElementsByTagName("titulo").item(0).setTextContent(nuevoTitulo);
+                System.out.println("Título modificado con éxito.");
+                return;
+            }
+        }
+        System.out.println("Receta no encontrada.");
+    }
+
+    private static void eliminarReceta(Document doc) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el título de la receta que desea eliminar: ");
+        String tituloEliminar = scanner.nextLine();
+
+        NodeList recetas = doc.getElementsByTagName("Receta");
+        for (int i = 0; i < recetas.getLength(); i++) {
+            Element receta = (Element) recetas.item(i);
+            String titulo = receta.getElementsByTagName("titulo").item(0).getTextContent();
+            if (titulo.equalsIgnoreCase(tituloEliminar)) {
+                doc.getDocumentElement().removeChild(receta);
+                System.out.println("Receta eliminada con éxito.");
+                return;
+            }
+        }
+        System.out.println("Receta no encontrada.");
+    }
+
+    private static void guardarDocumento(Document doc) {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(XML_FILE));
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            e.printStackTrace();
         }
     }
 }
+
